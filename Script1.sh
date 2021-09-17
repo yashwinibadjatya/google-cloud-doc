febc1778b2f36a30e8fcf8d5e4ac2798e9f5ca62
prateekbadjatya <41595806+prateekbadjatya@users.noreply.github.com>
2021-09-17 07:53:56 +0530

    Create Script1.sh

---
+++ Script1.sh
@@ -0,0 +1,12 @@
+sudo su
+apt update 
+apt install apache2
+ls /var/www/html
+echo "Hello World!"
+echo "Hello World!" > /var/www/html/index.html
+echo $(hostname)
+echo $(hostname -i)
+echo "Hello World from $(hostname)"
+echo "Hello World from $(hostname) $(hostname -i)"
+echo "Hello world from $(hostname) $(hostname -i)" > /var/www/html/index.html
+
